9dba526349b0c4c7232a397fff8ffbcaaf1120e5
Matt DaSilva <matt_das@hotmail.co.uk>
2022-04-04 16:18:27 +0100

    Refractor

--- src/test/java/com/techreturners/tic_tac_toe/TicTacToeTest.java
+++ src/test/java/com/techreturners/tic_tac_toe/TicTacToeTest.java
@@ -1,27 +1,34 @@
 package com.techreturners.tic_tac_toe;
 
 import com.techreturners.tic_tac_toe.app.TicTacToe;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TicTacToeTest {
 
+    TicTacToe game;
+
+    @BeforeEach
+    public void setUp(){
+        game = new TicTacToe();
+    }
+
     @Test
     public void testEmptyGridFirstMove() {
-        TicTacToe game = new TicTacToe();
         int gridPosition = 1;
         assertEquals(String.format("Player X played their turn in position %s", gridPosition), game.play(gridPosition));
     }
 
 
     @Test
-    public void testEmptyGridSecondMove(){
-        TicTacToe game = new TicTacToe();
+    public void testEmptyGridSecondMove() {
         int gridPosition = 1;
 
         game.play(2);
         assertEquals(String.format("Player O played their turn in position %s", gridPosition), game.play(gridPosition));
     }
+
 
 }
