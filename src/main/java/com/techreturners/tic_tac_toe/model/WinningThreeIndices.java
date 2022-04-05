74a81ed69250adca8a373e0eb374e0f204026c90
Matt DaSilva <matt_das@hotmail.co.uk>
2022-04-05 11:00:38 +0100

    Refractor WinningThreeIndices to make all methods protected

--- src/main/java/com/techreturners/tic_tac_toe/model/WinningThreeIndices.java
+++ src/main/java/com/techreturners/tic_tac_toe/model/WinningThreeIndices.java
@@ -4,25 +4,25 @@ public class WinningThreeIndices {
 
     private int[] wti = new int[3];
 
-    public WinningThreeIndices(int index1, int index2, int index3) {
+    protected WinningThreeIndices(int index1, int index2, int index3) {
         wti[0] = index1;
         wti[1] = index2;
         wti[2] = index3;
     }
 
-    public int getFirstIndex() {
+    protected int getFirstIndex() {
         return wti[0];
     }
 
-    public int getSecondIndex() {
+    protected int getSecondIndex() {
         return wti[1];
     }
 
-    public int getThirdIndex() {
+    protected int getThirdIndex() {
         return wti[2];
     }
 
-    public boolean contains(int lastPosition) {
+    protected boolean contains(int lastPosition) {
         for (int i : wti)
             if (i == lastPosition)
                 return true;
